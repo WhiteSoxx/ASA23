db5c97d39d84a7e111ed3c461c1ba687b32f245c
Guilherm Rocha <guilherme.rocha@tecnico.ulisboa.pt>
2023-11-25 12:19:52 +0000

    public tests passed,only 6/15 in ASA-Proj_Tester

--- proj1.cpp
+++ proj1.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <list>
 #include <vector>
@@ -7,56 +6,76 @@
 using namespace std;
 int _Xsize, _Ysize;
 int _NumPieces;
-std::list<std::tuple<int,int,int>> _pieces;
+int _horizontalCut[2];
+int _verticalCut[2];
+list<tuple<int,int,int>> _pieces;
 // tuple -> (x_size, y_size, price)
-int **_results;
+int _results[20][20];
 
 void readInput(){
     int x,y,price;
-    std::cin >> _Xsize; std::cin.ignore(); std::cin >> _Ysize;
-    std::cin >> _NumPieces;
-    for (size_t i = 0; i < _NumPieces; i++){
-        std::cin >> x; std::cin.ignore(); std::cin >> y; std::cin.ignore(); std::cin >> price;
-        _pieces.push_front(std::make_tuple(x,y,price));
+    cin >> _Xsize; cin.ignore(); cin >> _Ysize;
+    cin >> _NumPieces;
+    for (int i = 0; i < _NumPieces; i++){
+        cin >> x; cin.ignore(); cin >> y; cin.ignore(); cin >> price;
+        _pieces.push_front(make_tuple(x,y,price));
     }
 
 }
 // max{corte vertical, corte horizontal, vender(se possível)}
 //para cada elemento da lista
 int maxPrice(int x, int y){
-    if (_results[x-1][y-1] != NULL) return _results[x-1][y-1];
-    std::tuple<int,int,int> piece;
-    int price = 0;
-    std::tuple<int,int> horizontal_cut;
-    std::tuple<int,int> vertical_cut;
-    int maxfeuhrrehgbe;
-    for (std::list<std::tuple<int,int,int>>::iterator it = _pieces.begin(); it != _pieces.end(); ++it){
-        piece = *it;
-        
-        if ((x == std::get<0>(piece) && y == std::get<1>(piece)) ||
-            x == std::get<1>(piece) && y == std::get<0>(piece))
-                price = get<2>(piece);
-        //corte horizontal
-        if (x > std::get<0>(piece))
-            horizontal_cut = std::make_tuple(x - std::get<0>(piece) ,x - (x - std::get<0>(piece)));
-        if (y > std::get<1>(piece))
-            vertical_cut = std::make_tuple(y - std::get<1>(piece) ,y - (y - std::get<1>(piece)));
-        
-        //vertical ou horizontal
-        max(max(maxPrice(get<0>(horizontal_cut),y) + maxPrice(get<1>(horizontal_cut),y),
-        maxPrice(x,get<0>(vertical_cut)) + maxPrice(x,get<1>(vertical_cut))),price);
-            
+    if (_results[x-1][y-1] != -1) return _results[x-1][y-1];
+    int decider = 0 , price = 0, n = 1;
+    int max_Price = 0, max_PricePiece;
+    for (list<tuple<int,int,int>>::iterator it = _pieces.begin(); it != _pieces.end(); ++it){
+        // cout <<  n  << endl;
+        // cout << "----------" << endl;
+        if ((x == get<0>(*it) && y == get<1>(*it)) ||
+            (x == get<1>(*it) && y == get<0>(*it)))
+            price = get<2>(*it);
+        //horizontal cut
+        if (x > get<0>(*it)){
+            _horizontalCut[0] = x - get<0>(*it);
+            _horizontalCut[1] =  x - _horizontalCut[0];
+            decider++;
         }
-        //corte vertical
-        return _results[_Xsize][_Ysize];
+        //vertical cut
+        if (y > get<1>(*it)){
+            _verticalCut[0] = y - get<1>(*it);
+            _verticalCut[1] =  y - _verticalCut[0];
+            decider+=2;
+        }
+        // cout << x << endl;
+        // cout << y << endl;
+        // cout << "----------" << endl;
+        switch(decider){
+            case(0): max_PricePiece = price;
+                break;
+            case(1): max_PricePiece = max(maxPrice(_horizontalCut[0],y) + maxPrice(_horizontalCut[1],y),price);
+                break;
+            case(2): max_PricePiece = max(maxPrice(x,_verticalCut[0]) + maxPrice(x,_verticalCut[1]),price);
+                break;
+            case(3): max_PricePiece = max(max(maxPrice(_horizontalCut[0],y) + maxPrice(_horizontalCut[1],y),
+            maxPrice(x,_verticalCut[0]) + maxPrice(x,_verticalCut[1])),price);
+                break;
+            default: max_PricePiece = 0;
+        }
+        if (max_PricePiece > max_Price) max_Price = max_PricePiece;
+        decider = 0;
     }
+    _results[x-1][y-1] = max_Price;
+    //cout << "----------" << endl;
+    return _results[_Xsize-1][_Ysize-1];
+}
 
 int main(){
     readInput();
-    _results = new int*[_Xsize];
-    for (size_t i = 0; i < _Xsize; i++){
-        _results[i] = new int[_Ysize];
+    for (int i= 0; i < _Xsize; i++){
+        for (int j = 0; j < _Ysize; j++){
+            _results[i][j] = -1;
+        }
     }
-    maxPrice(_Xsize,_Ysize);
+    cout << maxPrice(_Xsize,_Ysize) << endl;
     return 0;
 }
