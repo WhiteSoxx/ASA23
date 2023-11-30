2e8e35bf06f8bbf2e60a77a7c443ea1ec83e9639
Guilherm Rocha <guilherme.rocha@tecnico.ulisboa.pt>
2023-11-30 23:16:13 +0000

    1600 Mooshak

--- proj1.cpp
+++ proj1.cpp
@@ -1,67 +1,60 @@
 #include <iostream>
-#include <unordered_map>
-#include <string>
 #include <array>
-#include <utility>
 #include <chrono>
 using namespace std;
 //using namespace chrono;
+// meter sem memoization (tentar)
+// guardar os valores na tabela calcular para todos os da tabela;
+// simplesmente iterar pela tabela-> já vou ter os valores para trás
 int _Xsize, _Ysize;
 int _NumPieces;
 struct Price{int price = -1;};
-unordered_map <string,int> _pieces;
 Price **_results;
-string key;
-string key_rot;
+
 void readInput(){
     int x,y,price;
     cin >> _Xsize; cin.ignore(); cin >> _Ysize;
     cin >> _NumPieces;
+    _results = new Price*[max(_Xsize,_Ysize)];
+    for (int i = 0; i < max(_Xsize,_Ysize); i++)
+        _results[i] = new Price[max(_Xsize,_Ysize)];
     for (int i = 0; i < _NumPieces; i++){
         cin >> x; cin.ignore(); cin >> y; cin.ignore(); cin >> price;
-        if (price > 0){
-            string dimensions = to_string(x) + "," + to_string(y);
-            //string dimensions_rot = to_string(y) + "," + to_string(x);
-            //if (_pieces.count(dimensions) > 0 || _pieces.count(dimensions_rot)) price = max(price,_pieces[dimensions]);
-            _pieces[dimensions] = price;
+        if (price > 0 && ((x <= _Xsize && y <= _Ysize) || (x <= _Ysize && y <=_Xsize))){
+            if (_results[x-1][y-1].price > 0 || _results[y-1][x-1].price > 0){
+                _results[x-1][y-1].price = max(max(_results[x-1][y-1].price, _results[y-1][x-1].price),price);
+                _results[y-1][x-1].price = -1;
+            }
+            else{_results[x-1][y-1].price = price;}
         }
     }
-
 }
-int maxPrice(int x, int y){
-    if (_results[x-1][y-1].price != -1) return _results[x-1][y-1].price;
-    if (_results[y-1][x-1].price != -1) return _results[y-1][x-1].price;
-    int max_price = 0;
-    key = to_string(x) + "," + to_string(y);
-    key_rot = to_string(y) + "," + to_string(x);
-    if (_pieces.count(key) > 0) max_price = _pieces[key];
-    else if(_pieces.count(key_rot) > 0) max_price = _pieces[key_rot];
+void maxPrice(int x, int y){
+    int price = 0,vertical_cut = 0,max_vertical_cut = 0, max_horizontal_cut = 0,horizontal_cut = 0;
+    if (_results[x-1][y-1].price != -1) price = _results[x-1][y-1].price;
+    if (_results[y-1][x-1].price != -1) price = _results[y-1][x-1].price;
     for (int k = 1; k < max(x,y); k++){
-        if (x > k)
-            max_price = max(max_price,maxPrice(k,y) + maxPrice(x-k,y));
-        if (y > k) 
-            max_price = max(max_price,maxPrice(x,k) + maxPrice(x,y-k));
+        if (x > k){
+            horizontal_cut = _results[k-1][y-1].price + _results[x-k-1][y-1].price;
+            max_horizontal_cut = (horizontal_cut > max_horizontal_cut) ? horizontal_cut : max_horizontal_cut;
+        }
+        if (y > k){
+            vertical_cut = _results[x-1][k-1].price + _results[x-1][y-k-1].price;
+            max_vertical_cut = (vertical_cut > max_vertical_cut) ? vertical_cut : max_vertical_cut;
+        }
     }
-    _results[x-1][y-1].price = max_price;
-    _results[y-1][x-1] = _results[x-1][y-1];
-    return max_price;
+    _results[x-1][y-1].price = max(max(price,max_horizontal_cut),max_vertical_cut);
 }
 
 int main(){
     //auto start_time = high_resolution_clock::now();
     readInput();
-    _results = new Price*[max(_Xsize,_Ysize)];
-    for (int i = 0; i < max(_Xsize,_Ysize); i++){
-        _results[i] = new Price[max(_Xsize,_Ysize)];
-    }
-    // for (int i = 0; i < max(_Xsize, _Ysize); i++){
-    //     for (int j = 0; j < max(_Xsize, _Ysize); j++){
-    //         _results[i][j] = -1;
-    //     }
-    // }
-    cout << maxPrice(_Xsize,_Ysize) << endl;
+    for (int i = 0; i < _Xsize; i++)
+        for(int j = 0; j < _Ysize; j++)
+            maxPrice(i+1,j+1);
+    cout << _results[_Xsize-1][_Ysize-1].price << endl;
     // auto end_time = high_resolution_clock::now();
     // auto duration = duration_cast<milliseconds>(end_time - start_time);
-    // cout << "it took " << duration.count()<<endl; 
+    // cout << "it took " << duration.count()<<endl;
     return 0;
 }
