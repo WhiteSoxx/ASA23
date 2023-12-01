f7947bbbba7a88443f764faaa18feaf995295183
Guilherm Rocha <guilherme.rocha@tecnico.ulisboa.pt>
2023-12-01 16:48:35 +0000

    1650 Mooshak (42 time limit)

--- proj1.cpp
+++ proj1.cpp
@@ -8,7 +8,10 @@ using namespace std;
 // simplesmente iterar pela tabela-> já vou ter os valores para trás
 int _Xsize, _Ysize;
 int _NumPieces;
-struct Price{int price = -1;};
+struct Price{
+    int price = 0;
+    int done = false;
+};
 Price **_results;
 
 void readInput(){
@@ -17,23 +20,24 @@ void readInput(){
     cin >> _NumPieces;
     _results = new Price*[max(_Xsize,_Ysize)];
     for (int i = 0; i < max(_Xsize,_Ysize); i++)
-        _results[i] = new Price[max(_Xsize,_Ysize)];
+        _results[i] = new Price[max(_Xsize,_Ysize)] ;
     for (int i = 0; i < _NumPieces; i++){
         cin >> x; cin.ignore(); cin >> y; cin.ignore(); cin >> price;
-        if (price > 0 && ((x <= _Xsize && y <= _Ysize) || (x <= _Ysize && y <=_Xsize))){
+       if (price > 0 && ((x <= _Xsize && y <= _Ysize) || (x <= _Ysize && y <=_Xsize))){
             if (_results[x-1][y-1].price > 0 || _results[y-1][x-1].price > 0){
                 _results[x-1][y-1].price = max(max(_results[x-1][y-1].price, _results[y-1][x-1].price),price);
-                _results[y-1][x-1].price = -1;
+                _results[y-1][x-1].price = _results[x-1][y-1].price;
             }
             else{_results[x-1][y-1].price = price;}
         }
+        
     }
 }
 void maxPrice(int x, int y){
-    int price = 0,vertical_cut = 0,max_vertical_cut = 0, max_horizontal_cut = 0,horizontal_cut = 0;
-    if (_results[x-1][y-1].price != -1) price = _results[x-1][y-1].price;
-    if (_results[y-1][x-1].price != -1) price = _results[y-1][x-1].price;
-    for (int k = 1; k < max(x,y); k++){
+    int limit = max(x,y)/2,vertical_cut = 0,max_vertical_cut = 0, max_horizontal_cut = 0,horizontal_cut = 0;
+    // if (_results[x-1][y-1].price != -1) price = _results[x-1][y-1].price;
+    // if (_results[y-1][x-1].price != -1) price = _results[y-1][x-1].price;
+    for (int k = 1; k <= limit; k++){
         if (x > k && k <= x/2){
             horizontal_cut = _results[k-1][y-1].price + _results[x-k-1][y-1].price;
             max_horizontal_cut = (horizontal_cut > max_horizontal_cut) ? horizontal_cut : max_horizontal_cut;
@@ -43,14 +47,32 @@ void maxPrice(int x, int y){
             max_vertical_cut = (vertical_cut > max_vertical_cut) ? vertical_cut : max_vertical_cut;
         }
     }
-    _results[x-1][y-1].price = max(max(price,max_horizontal_cut),max_vertical_cut);
+    _results[x-1][y-1].price = max(max(max(_results[x-1][y-1].price,max_horizontal_cut),max_vertical_cut),_results[y-1][x-1].price);
+    _results[x-1][y-1].done = true;
+    if ((y > 1 || x > 1) && y <= _Xsize && x <= _Ysize && x != y){
+        _results[y-1][x-1].price = _results[x-1][y-1].price;
+        _results[y-1][x-1].done = true;
+    }
 }
 int main(){
     //auto start_time = high_resolution_clock::now();
     readInput();
-    for (int i = 0; i < _Xsize; i++)
-        for(int j = 0; j < _Ysize; j++)
-            maxPrice(i+1,j+1);
+    for (int i = 0; i < _Xsize ; i++){
+        for(int j = 0; j < _Ysize; j++){
+           if (!_results[i][j].done) maxPrice(i+1,j+1);
+        }
+    }
+    // lines = (_Xsize > _Ysize) ? _Xsize - _Ysize : 0;
+    // colums = (_Xsize > _Ysize) ? 0 : _Ysize - _Xsize;
+    // for (int i = lines; i < _Xsize ; i++)
+    //     for (int j = colums; j < _Ysize; j++)
+    //         maxPrice(i+1,j+1);
+    
+    // for (int i = 0; i < _Xsize; i++){
+    //     for(int j = 0; j < _Ysize; j++)
+    //         cout << _results[i][j].price << " ";
+    //     cout << endl;
+    //}
     cout << _results[_Xsize-1][_Ysize-1].price << endl;
     // auto end_time = high_resolution_clock::now();
     // auto duration = duration_cast<milliseconds>(end_time - start_time);
