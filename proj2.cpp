dd71a9c293c034e8c2c87b0694b009deea5cc984
Guilherm Rocha <guilherme.rocha@tecnico.ulisboa.pt>
2023-12-17 18:22:20 +0000

    second dfs done

--- proj2.cpp
+++ proj2.cpp
@@ -9,7 +9,8 @@
 
 using namespace std;
 int _V,_E;
-
+int _time = 0,_end;
+bool _first_dfs = true;
 struct Vertice{
     int parent;
     int color = WHITE;
@@ -18,74 +19,71 @@ struct Vertice{
     int scc;
 };
 
-Vertice* _vertices;
-Vertice* _vertices_second;
+Vertice * _first_vertices;
+Vertice * _second_vertices;
+int * _first_order;
+int * _second_order;
 list<int>* graph;
 list<int>* graphT;
 void readGraph(){
     int u,v;
     scanf("%d %d",&_V,&_E);
+    _end = _V;
     graph = new list<int>[_V+1];
     graphT = new list<int>[_V+1];
-    _vertices = new Vertice[_V+1];
-    _vertices_second = new Vertice[_V+1];
+    _first_vertices = new Vertice[_V+1];
+    _second_vertices = new Vertice[_V+1];
+    _first_order = new int[_V+1];
+    _second_order = new int[_V+1];
     for (int i = 1; i <= _E; i++){
+        if (i <= _V) _first_order[i] = i;
         scanf("%d %d",&u,&v);
         graph[u].push_front(v);
         graphT[v].push_front(u);
     }
 }
-void Dfs_Visit(list<int>* local_graph, int time,int end, stack<int> stack,Vertice* vertices) {
+void Dfs_Visit(list<int>* local_graph, stack<int> stack,Vertice* vertices) {
     while(!stack.empty()){
         int V = stack.top();
         if (vertices[V].color == WHITE){ 
-            time++;
+            _time++;
             vertices[V].color = GRAY;
-            vertices[V].find_time = time;
+            vertices[V].find_time = _time;
             for(list<int>::iterator it = local_graph[V].begin(); it != local_graph[V].end();it++)
                 stack.push(*it);
         }else{
             if (vertices[V].color == GRAY){
-                time++;
+                _time++;
                 vertices[V].color = BLACK;
-                vertices[V].end_time = time;
-                _vertices_second[end] = vertices[V];
-                end--;
+                vertices[V].end_time = _time;
+                if (_first_dfs){
+                    _second_order[_end] = V;
+                    _end--;
+                }
             }
             stack.pop();
         }
     }
     //cout << endl;
 }
-void Dfs(list<int>* local_graph,Vertice * vertices){
-    int time = 0,end = _V;
+void Dfs(list<int>* local_graph,Vertice * vertices,int * order){
     stack<int> stack;
-    for (int vert = 1; vert <= _V; vert++)
-        if (!vertices[vert].color){ // WHITE
+    for (int i = 1; i <= _V; i++)
+        if (!vertices[order[i]].color){ // WHITE
         //if GRAY it's in the stack already
-            stack.push(vert);
-            Dfs_Visit(local_graph,time,end,stack,vertices);
+            stack.push(order[i]);
+            Dfs_Visit(local_graph,stack,vertices);
         }
 }
 
 int main(){
     readGraph();
-    //Vertice V = Vertice();
-    //V.number = 3;
-    //dfs(V);
-    // for (int i = 1; i <= _V; i++){
-    //     cout << i << ": ";
-    //     for (list<int>::iterator it = graph[i].begin(); it != graph[i].end();it++){
-    //         cout << *it << " ";
-    //         cout << "color->";
-    //         cout << vertices[*it].color << ";";
-    //     }
-    //     cout << endl;
-    // }
-    Dfs(graph,_vertices);
-    Dfs(graphT,_vertices_second);
+    Dfs(graph,_first_vertices,_first_order);
+    _time = 0;
+    _first_dfs = false;
+    Dfs(graphT,_second_vertices,_second_order);
     for (int i = 1; i <= _V; i++){
-        cout << i << "-> " << _vertices[i].find_time << "/" << _vertices[i].end_time << endl;
+        printf("%d -> %d/%d\n",_second_order[i],_second_vertices[_second_order[i]].find_time,_second_vertices[_second_order[i]].end_time);
     }
     return 0;
 }
