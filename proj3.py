1d77f3c0a1b312694747c4ddabd745d36d50860e
Guilherm Rocha <guilherme.rocha@tecnico.ulisboa.pt>
2024-01-07 17:51:47 +0000

    1700 Mooshak

--- proj3.py
+++ proj3.py
@@ -19,19 +19,19 @@ for i in range(1,num_toys+num_bundles+1):
         toys_bundles.setdefault(toy1, []).append(i)
         toys_bundles.setdefault(toy2, []).append(i)
         toys_bundles.setdefault(toy3, []).append(i)
-        variables[i] = LpVariable("",0,min(min(capacity[toy1],capacity[toy2]),capacity[toy3]),cat = LpInteger)
+        variables[i] = LpVariable("",0,min(min(capacity[toy1],capacity[toy2]),capacity[toy3])+1,cat = LpInteger)
 
 vars = LpVariable.dict("", variables, 0, None, LpInteger)
 
 prob += lpSum([vars[i] * profit[i] for i in range(1,num_bundles+num_toys+1)])
-prob += lpSum([vars[i] * (3 if i > num_toys else 1) for i in range(1,num_bundles+num_toys+1)]) <= max_toys
+prob += lpSum([vars[i] * (3 if i > num_toys else 1) for i in range(1,num_bundles+num_toys+1)]) <= max_toys,"bas"
 #bundle has 3 toys
 
 for t_b in range(1,num_toys+1):
     if t_b in toys_bundles: # at least one bundle exists with the toy
-        prob += (vars[t_b] + lpSum(vars[toys_bundles[t_b][i]] for i in range(len(toys_bundles[t_b])))) <= capacity[t_b]                        
+        prob += (vars[t_b] + lpSum(vars[toys_bundles[t_b][i]] for i in range(len(toys_bundles[t_b])))) <= capacity[t_b]    ,f"aasdf{t_b}"                    
     else:
-        prob += (vars[t_b]) <= capacity[t_b]
+        prob += (vars[t_b]) <= capacity[t_b],f"aasdf{t_b}"
 
 
 prob.solve(GLPK(msg=0))
